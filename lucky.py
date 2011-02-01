fddcc2f992789ba64dbe14f20fe647978d32e754
Tim Horton <hortont424@gmail.com>
2011-02-01 15:25:03 -0500

    clean up a lot of sillyness

--- lucky.py
+++ lucky.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 
 # Copyright 2011 Peter Hajas. All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without modification, are
 # permitted provided that the following conditions are met:
-# 
+#
 #    1. Redistributions of source code must retain the above copyright notice, this list of
 #       conditions and the following disclaimer.
-# 
+#
 #    2. Redistributions in binary form must reproduce the above copyright notice, this list
 #       of conditions and the following disclaimer in the documentation and/or other materials
 #       provided with the distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY PETER HAJAS ``AS IS'' AND ANY EXPRESS OR IMPLIED
 # WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
 # FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL PETER HAJAS OR
@@ -21,7 +21,7 @@
 # ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# 
+#
 # The views and conclusions contained in the software and documentation are those of the
 # authors and should not be interpreted as representing official policies, either expressed
 # or implied, of Peter Hajas.
@@ -49,20 +49,13 @@ historyPath = os.environ['HOME'] + "/.bash_history"
 history = open(historyPath)
 
 # Read into a list
-list = []
+list = reversed([l for l in history.readlines() if l != ""])
 
-while 1:
-	string = history.readline()
-	if string == "":
-		break
-	list.append(string)
+# Find matching entries
+matches = [entry for entry in list if search in entry]
 
-list.reverse()
-
-for entry in list:
-	if search in entry:
-		print "Found it!"
-		os.system(entry)
-		quit()
-		
-print "Didn't find what you were looking for, you weren't lucky :("
+if len(matches):
+	print "Found it: {0}".format(matches[0])
+	os.system(matches[0])
+else:
+	print "Didn't find what you were looking for, you weren't lucky :("
